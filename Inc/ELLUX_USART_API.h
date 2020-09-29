b56485853fa710fb7c2b6e10106504152fc1f4f0
Osamich <arbinadunchev@gmail.com>
2020-09-29 19:43:33 +0300

    --Fixed commentary and else.

--- Inc/ELLUX_USART_API.h
+++ Inc/ELLUX_USART_API.h
@@ -1,27 +1,27 @@
 /* Write a public API to interact with a hardware UART (USART) block, which should include:
 
-                1. Initialization of the module;
+1. Initialization of the module;
 
-                2. Setting the baud rate;
+2. Setting the baud rate;
 
-                3. Setting the parity bit;
+3. Setting the parity bit;
 
-                4. Setting the duration of the stop bit;
+4. Setting the duration of the stop bit;
 
-                5. Setting the callback function for the receiver / transmitter;
+5. Setting the callback function for the receiver / transmitter;
 
-                6. Data array transfer function. Data array will be sent by the transmitter;
+6. Data array transfer function. Data array will be sent by the transmitter;
 
 
 If the requirements described above are not enough to ensure the functionality of the module, then other parameters can be passed at your discretion.
 
 Additionally:
 
-                1. The hardware block only works with interrupts;
+1. The hardware block only works with interrupts;
 
-                2. The software module must be able to work with several hardware blocks (USART1, USART2, etc.);
+2. The software module must be able to work with several hardware blocks (USART1, USART2, etc.);
 
-                3. Public API should consist of only one header file (platform-specific source files are not needed); */
+3. Public API should consist of only one header file (platform-specific source files are not needed); */
 
 #ifndef ELLUX_USART_API_H
 #define ELLUX_USART_API_H
@@ -66,13 +66,20 @@ typedef enum
 */
 typedef struct _ELLUX_USART_INIT_T
 {
-    uint32_t BaudRate;
-    uint32_t StopBitDuration;
-    ellux_usart_parity_t ParityBit;
-    ellux_usart_mode_t mode;
-    ellux_usart_hwblock_t HwBlock;
-    bool RxEnabled;
-    bool TxEnabled;
+    uint32_t BaudRate; // BaudRate for asynchronous communication
+
+    uint32_t StopBitDuration; // Number in baud ticks of stop bit length
+
+    ellux_usart_parity_t ParityBit; // Parity bit checking mode
+
+    ellux_usart_mode_t Mode;// UART/USART
+
+    ellux_usart_hwblock_t HwBlock; // See above
+
+    bool RxEnabled;// Condition to set reception
+
+    bool TxEnabled;// Condition to set transmission
+
 } ellux_usart_init_t;
 /*
     Forward declaration of main handle struct
@@ -107,58 +114,75 @@ typedef void (ellux_usart_callback_t)(ellux_usart_handle_t* const uarthandler);
 struct _ELLUX_USART_HANDLE_T
 {
     ellux_usart_hwblock_t HwBlock;
+
     bool IsInited; // this variable may be useful if user want to change options after
                    // initialisation, for example to restart USART module to make changes
-    uint8_t* TxBufPtr;
-    uint32_t TxBufSize;
-    uint32_t TxCounter;
-    ellux_usart_callback_t* TxCallbackPtr;
-    uint8_t* RxBufPtr;
-    uint32_t RxBufSize;
-    uint32_t RxCounter;
-    ellux_usart_callback_t* RxCallbackPtr;
+
+    ellux_usart_status_t ErrorCode;// 
+
+    uint8_t* TxBufPtr; //Pointer to buffer to transmit
+
+    uint32_t TxBufSize; // Stores length of message to transmit
+
+    uint32_t TxCounter; // Counts how many bytes was transffered, needed to invoke Txcallback
+
+    ellux_usart_callback_t* TxCallbackPtr; // pointer to transmission callback function
+
+    uint8_t* RxBufPtr;//Pointer to buffer where to recieve
+
+    uint32_t RxBufSize;// Stores length of message to recieve
+
+    uint32_t RxCounter;// Counts how many bytes was recieved, needed to invoke Rxcallback
+
+    ellux_usart_callback_t* RxCallbackPtr; // pointer to reception callback function
     
 };
 
 /*
-    Create init struct with default parameters 
-    After that, user must change them accordingly his preferences
-    Returns status code
+    Fills in init struct with default parameters 
+    After that, user must change them accordingly to his preferences
 */
 void ELLUX_USART_DefaultConstruct(ellux_usart_init_t* const uartinit);
 
 /*
     Initialisation of given USART/UART module 
-    User must set all init options or it'll return error
+    fields belonging to transmitter/reciever,are initialised with NULL
     Returns status code
 */
 ellux_usart_status_t ELLUX_USART_Init(ellux_usart_handle_t* const uarthandler, const ellux_usart_init_t* const uartinit);
 
 /*
-    Denitialisation of given USART/UART module 
+    Denitialisation of given USART/UART module,
+    Stopping all communications
     Returns status code
 */
 ellux_usart_status_t ELLUX_USART_DeInit(ellux_usart_handle_t* const uarthandler);
 
 
 /*
-    Set callback function of given usart module to invoke after transmit
-    Returns status code
+    Set callback function of given usart module to invoke upon end of transmission
+    Must return error USART_STATUS_NOT_INITIALISED if wasn't invoked init function or/and flag TxEnabled was false
 */
 ellux_usart_status_t ELLUX_USART_SetTxCallback(ellux_usart_handle_t* const uarthandler, ellux_usart_callback_t* const txcallback);
 
 /*
-    Set callback function of given usart module to invoke upon recieve
-    Returns status code
+    Set callback function of given usart module to invoke upon end of reception
+    Must return error USART_STATUS_NOT_INITIALISED if wasn't invoked init function or/and flag RxEnabled was false
 */
 ellux_usart_status_t ELLUX_USART_SetRxCallback(ellux_usart_handle_t* const uarthandler, ellux_usart_callback_t* const rxcallback);
 
 /*
-    Transmit function, data array must be allocated or declared statically
-    Definitely returns error if transmitter is off
+    Start transmiting function
+    After transmitting full length, callback'll be invoked
+    Must return error USART_STATUS_NOT_INITIALISED if transmitter is off
 */
 ellux_usart_status_t ELLUX_USART_StartTransmit(ellux_usart_handle_t* const uarthandler, const uint8_t* const data, const uint32_t length);
 
+/*
+    Start recieveing function, data array must be preallocated
+    After recieving full length, callback'll be invoked
+    Must return error USART_STATUS_NOT_INITIALISED if reciever is off
+*/
 ellux_usart_status_t ELLUX_USART_StartRecieve(ellux_usart_handle_t* const uarthandler, uint8_t* const data, const uint32_t length);
 
 #endif // ELLUX_USART_API_H
--- Test/Test.c
+++ Test/Test.c
@@ -4,38 +4,41 @@
 
 Data for hardware block 1 (USART1):
 
-                1. Baud rate is 9600;
+1. Baud rate is 9600;
 
-                2. Receives data only (RX line is active);
+2. Receives data only (RX line is active);
 
  
 
 Data for hardware block 2 (USART2):
 
-                1. Baud rate is 115200;
+1. Baud rate is 115200;
 
-                2. Transmits data only (TX line is active);
+2. Transmits data only (TX line is active);
 
  
 
 General data for the second task:
 
-                1. After receiving data via USART1, they must be saved and sent via USART2;
+1. After receiving data via USART1, they must be saved and sent via USART2;
 
-                2. Transmission via USART2 is carried out in blocks of 128 bytes, so enough data must be accumulated before sending via USART2;
+2. Transmission via USART2 is carried out in blocks of 128 bytes, so enough data must be accumulated before sending via USART2;
 
-                3. At the moment of transmission of the data block via USART2, it is possible to receive new data via USART1, so new / transmitted data should not be lost or damaged;
+3. At the moment of transmission of the data block via USART2, it is possible to receive new data via USART1, so new / transmitted data should not be lost or damaged;
 
-                4. To check the functionality, a test should be created (the type of the test is at your discretion); */
+4. To check the functionality, a test should be created (the type of the test is at your discretion); */
 
 #include "ELLUX_USART_API.h"
 #include <string.h>
+#include <assert.h>
 
-#define MAX_BUFFERS 2
+#define MAX_BUFFERS 2 // Represents length of queue for trasmitter/resiver,
+                      // presumably it should be enough to handle given task, but it can be easily increased
 
 ellux_usart_handle_t Uart1, Uart2;
 uint8_t Buffers[MAX_BUFFERS][128];
 uint8_t RxIndex=0, TxIndex=0;
+ellux_usart_status_t ErrorCode;
 bool Recieved=false, Transmitted = false, Transmitting=false;
 
 static void Uart1Init();
@@ -48,50 +51,58 @@ int main(void)
 
     Uart1Init();
     Uart2Init();
-    ELLUX_USART_Recieve(&Uart1, Buffers[RxIndex], 128);
+    ErrorCode =ELLUX_USART_StartRecieve(&Uart1, Buffers[RxIndex], 128);
+    assert(ErrorCode==0);
     while(1)
     {
         if(Recieved)
         {
             Recieved=false;
-            ELLUX_USART_Transmit(&Uart2,Buffers[TxIndex],128);
+            Transmitted =false;
+            ErrorCode =ELLUX_USART_StartTransmit(&Uart2,Buffers[TxIndex],128);
+            assert(ErrorCode==0);
             TxIndex = (TxIndex+1) % MAX_BUFFERS;
         }
-        if(Transmitted)
+        if(Transmitted && ((TxIndex+1)%MAX_BUFFERS < RxIndex))
         {
             Transmitted=false;
-            
+            ErrorCode =ELLUX_USART_StartTransmit(&Uart2,Buffers[TxIndex],128);
+            assert(ErrorCode==0);
+            TxIndex = (TxIndex+1) % MAX_BUFFERS;
         }
+        // To not burn down our MCU,it's recommended to insert sleep(1 ms) here
     }
 }
 
 static void Uart1Init()
 {
     ellux_usart_init_t Uart1Init;
-    ELLUX_USART_Create(&Uart1Init);
+    ELLUX_USART_DefaultConstruct(&Uart1Init);
     Uart1Init.BaudRate= 9600;
     Uart1Init.HwBlock = ELLUX_USART1;
-    Uart1Init.mode = ELLUX_USART_MODE_UART;
+    Uart1Init.Mode = ELLUX_USART_MODE_UART;
     Uart1Init.ParityBit = PARITY_BIT_NONE;
     Uart1Init.StopBitDuration= 1;
     Uart1Init.TxEnabled = false;
     Uart1Init.RxEnabled = true;
-    ELLUX_USART_Init(&Uart1, &Uart1Init);
+    ErrorCode = ELLUX_USART_Init(&Uart1, &Uart1Init);
+    assert(ErrorCode==0); // assertion to check initialisation
     ELLUX_USART_SetRxCallback(&Uart1, &Uart1RxCallback);
 }
 
 static void Uart2Init()
 {
     ellux_usart_init_t Uart2Init;
-    ELLUX_USART_Create(&Uart2Init);
+    ELLUX_USART_DefaultConstruct(&Uart2Init);
     Uart2Init.BaudRate= 115200;
     Uart2Init.HwBlock = ELLUX_USART2;
-    Uart2Init.mode = ELLUX_USART_MODE_UART;
+    Uart2Init.Mode = ELLUX_USART_MODE_UART;
     Uart2Init.ParityBit = PARITY_BIT_NONE;
     Uart2Init.StopBitDuration= 1;
     Uart2Init.TxEnabled = true;
     Uart2Init.RxEnabled = false;
-    ELLUX_USART_Init(&Uart2, &Uart2Init);
+    ErrorCode = ELLUX_USART_Init(&Uart2, &Uart2Init);
+    assert(ErrorCode==0);// assertion to check initialisation
     ELLUX_USART_SetTxCallback(&Uart2, &Uart2TxCallback);
 }
 
@@ -99,10 +110,10 @@ static void Uart1RxCallback(ellux_usart_handle_t* const uarthandler )
 {
     Recieved=true;
     RxIndex = (RxIndex+1) % MAX_BUFFERS;
-    ELLUX_USART_Recieve(&Uart1,Buffers[RxIndex],128);
+    ErrorCode = ELLUX_USART_StartRecieve(&Uart1,Buffers[RxIndex],128);
+    assert(ErrorCode==0);
 }
 static void Uart2TxCallback(ellux_usart_handle_t* const uarthandler )
 {
     Transmitted = true;
-    
 }
